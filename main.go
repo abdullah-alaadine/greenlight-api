97a44cc824790052d0ee1e3487314b8082fff907
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-14 05:24:00 +0300

    call StartPrompt in main.go

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/abdullah-alaadine/sql-lite/cli"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 )
@@ -11,4 +12,5 @@ import (
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 	log.Info().Msg("Hello, world!")
+	cli.StartPrompt("randomName")
 }
