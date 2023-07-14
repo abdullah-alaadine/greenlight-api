2d1a05fd444c5e32840a00bb89e67c69935cb8ea
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-07-14 03:51:27 +0300

    add github.com/rs/zerolog

--- main.go
+++ main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
+	log.Info().Msg("Hello, world!")
+}
