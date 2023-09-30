f9c89161b0f028c8870b9411b6fa54ea715a825a
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-09-30 22:19:30 +0300

    update handlers to encode envelope type instead of interface{} directly

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -20,7 +20,7 @@ func (app *application) readIDParam(r *http.Request) (int64, error) {
 	return id, nil
 }
 
-func (app *application) writeJSON(w http.ResponseWriter, status int, data interface{}, headers http.Header) error {
+func (app *application) writeJSON(w http.ResponseWriter, status int, data envelope, headers http.Header) error {
 	js, err := json.MarshalIndent(data, "", "\t")
 	if err != nil {
 		return err
