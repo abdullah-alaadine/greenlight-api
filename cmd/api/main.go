a79bd4267af837aeb385536f87b99542ba524a55
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-04 06:22:21 +0300

    set the connection-pool settings in openDB func

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -59,7 +59,7 @@ func main() {
 	}
 
 	srv := &http.Server{
-			Addr:         fmt.Sprintf(":%d", cfg.port),
+		Addr:         fmt.Sprintf(":%d", cfg.port),
 		Handler:      app.routes(),
 		IdleTimeout:  time.Minute,
 		ReadTimeout:  10 * time.Second,
@@ -76,6 +76,16 @@ func openDB(cfg config) (*sql.DB, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+
+	duration, err := time.ParseDuration(cfg.db.maxIdleTime)
+	if err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxIdleTime(duration)
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
