ab2a2921feadffc8c16f3ff0d618fc7dd68dc5ee
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-17 19:51:01 +0300

    level implements the Stringer interface

--- internal/jsonlog/jsonlog.go
+++ internal/jsonlog/jsonlog.go
@@ -8,3 +8,16 @@ const (
 	LevelFatal
 	LevelOff
 )
+
+func (l Level) String() string {
+	switch l {
+	case LevelInfo:
+		return "INFO"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	default:
+		return ""
+	}
+}
