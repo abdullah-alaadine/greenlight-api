970fa59e6131fd6a9711fb2569c402cd8d584dee
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-19 23:40:46 +0300

    create ValidatePasswordPlaintext helper func

--- internal/data/users.go
+++ internal/data/users.go
@@ -50,3 +50,9 @@ func ValidateEmail(v *validator.Validator, email string) {
 	v.Check(email != "", "email", "must be provided")
 	v.Check(validator.Matches(email, validator.EmailRX), "email", "must be a valid email address")
 }
+
+func ValidatePasswordPlaintext(v *validator.Validator, password string) {
+	v.Check(password != "", "password", "must be provided")
+	v.Check(len(password) >= 8, "password", "must be at least 8 bytes long")
+	v.Check(len(password) <= 72, "password", "must not be more than 72 bytes long")
+}
