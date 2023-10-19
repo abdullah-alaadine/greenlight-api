c768b9b909533f0fcb36d666f7c85e1f382c5341
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-19 23:27:54 +0300

    create (*password) Matches method

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"errors"
 	"time"
 
 	"golang.org/x/crypto/bcrypt"
@@ -29,4 +30,17 @@ func (p *password) Set(plaintextPassword string) error {
 	p.plaintext = &plaintextPassword
 	p.hash = hash
 	return nil
+}
+
+func (p *password) Matches(plaintextPassword string) (bool, error) {
+	err := bcrypt.CompareHashAndPassword(p.hash, []byte(plaintextPassword))
+	if err != nil {
+		switch {
+		case errors.Is(err, bcrypt.ErrMismatchedHashAndPassword):
+			return false, nil
+		default:
+			return false, err
+		}
+	}
+	return true, nil
 }
