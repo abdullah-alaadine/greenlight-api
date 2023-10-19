68349cf999a7e5fbf1357a3442257877453a88ca
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-19 23:23:16 +0300

    create User type

--- internal/data/users.go
+++ internal/data/users.go
@@ -1,5 +1,17 @@
 package data
 
+import "time"
+
+type User struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  password  `json:"-"`
+	Activated bool      `json:"activated"`
+	Version   int       `json:"-"`
+}
+
 type password struct {
 	plaintext *string
 	hash      []byte
