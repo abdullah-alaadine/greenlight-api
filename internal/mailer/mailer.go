a92a571c314f457d2c4b0904f924a85d6e51d78c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-22 17:09:23 +0300

    create Mailer method, Send

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -1,7 +1,9 @@
 package mailer
 
 import (
+	"bytes"
 	"embed"
+	"html/template"
 	"time"
 
 	"github.com/go-mail/mail/v2"
@@ -23,4 +25,42 @@ func New(host string, port int, username, password, sender string) Mailer {
 		dialer: dialer,
 		sender: sender,
 	}
+}
+
+func (m Mailer) Send(recipient, templateFile string, data interface{}) error {
+	tmpl, err := template.New("email").ParseFS(templateFS, "templates/"+templateFile)
+	if err != nil {
+		return err
+	}
+
+	subject := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(subject, "subject", data)
+	if err != nil {
+		return err
+	}
+
+	plainBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(plainBody, "plainBody", data)
+	if err != nil {
+		return err
+	}
+
+	htmlBody := new(bytes.Buffer)
+	err = tmpl.ExecuteTemplate(htmlBody, "htmlBody", data)
+	if err != nil {
+		return err
+	}
+
+	msg := mail.NewMessage()
+	msg.SetHeader("To", recipient)
+	msg.SetHeader("From", m.sender)
+	msg.SetHeader("Subject", subject.String())
+	msg.SetBody("text/plain", plainBody.String())
+	msg.AddAlternative("text/html", htmlBody.String())
+
+	err = m.dialer.DialAndSend(msg)
+	if err != nil {
+		return err
+	}
+	return nil
 }
