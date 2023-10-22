345c46fa8abf25c3d41d40593e6a114914232900
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-22 17:05:35 +0300

    create Mailer constructor function

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -2,6 +2,7 @@ package mailer
 
 import (
 	"embed"
+	"time"
 
 	"github.com/go-mail/mail/v2"
 )
@@ -12,4 +13,14 @@ var templateFS embed.FS
 type Mailer struct {
 	dialer *mail.Dialer
 	sender string
+}
+
+func New(host string, port int, username, password, sender string) Mailer {
+	dialer := mail.NewDialer(host, port, username, password)
+	dialer.Timeout = 5 * time.Second
+
+	return Mailer{
+		dialer: dialer,
+		sender: sender,
+	}
 }
