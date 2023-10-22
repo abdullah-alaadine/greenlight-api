22d45de35f64fa9d1c6eb23e02fa1e237088c273
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-22 17:04:25 +0300

    create Mailer type

--- internal/mailer/mailer.go
+++ internal/mailer/mailer.go
@@ -1,6 +1,15 @@
 package mailer
 
-import "embed"
+import (
+	"embed"
+
+	"github.com/go-mail/mail/v2"
+)
 
 //go:embed "templates"
 var templateFS embed.FS
+
+type Mailer struct {
+	dialer *mail.Dialer
+	sender string
+}
