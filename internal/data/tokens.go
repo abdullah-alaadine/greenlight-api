b58459af6518095dbb5b310a9411a106426155ff
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-10-24 23:00:39 +0300

    create generateToken helper

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,6 +1,11 @@
 package data
 
-import "time"
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base32"
+	"time"
+)
 
 const (
 	ScopeActivation = "activation"
@@ -12,4 +17,25 @@ type Token struct {
 	UserID    int64
 	Expiry    time.Time
 	Scope     string
+}
+
+func generateToken(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := &Token{
+		UserID: userID,
+		Expiry: time.Now().Add(ttl),
+		Scope:  scope,
+	}
+
+	randomBytes := make([]byte, 16)
+
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	token.Plaintext = base32.StdEncoding.WithPadding(base32.NoPadding).EncodeToString(randomBytes)
+
+	hash := sha256.Sum256([]byte(token.Plaintext))
+	token.Hash = hash[:]
+	return token, nil
 }
